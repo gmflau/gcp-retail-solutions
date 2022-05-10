9e06e199ba1a8cd9327fa097822d3559946c8f83
gmflau <gmflau@gmail.com>
2022-05-10 13:53:09 -0700

    Remove creds

--- cloudrun-redis/redis_config.py
+++ cloudrun-redis/redis_config.py
@@ -4,7 +4,6 @@ import redis
 redishost = os.environ.get("REDISHOST", "localhost")
 redisport = os.environ.get("REDISPORT", "6379")
 redispassword = os.environ.get("REDISPASSWORD", "password")
-#r = redis.StrictRedis(host='redis-14897.c280.us-central1-2.gce.cloud.redislabs.com', port=14897, password='SceKGdmO9JwoDuUmEBv6QEWV6do5ADp2', db=0)
 r =  redis.StrictRedis(host=redishost, port=redisport, password=redispassword, db=0)
 r.set('rolex','daytona-gold')
 
