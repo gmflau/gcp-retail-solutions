7a77accc97a6d28dee64bd31b29235077402f1fc
gmflau <gmflau@gmail.com>
2022-05-27 00:48:49 -0700

    Update run.sh

--- gcloud/run.sh
+++ gcloud/run.sh
@@ -35,8 +35,8 @@ export vpc_connector_west=glau-retail-us-west1-09
 
 
 export app_image=gcr.io/central-beach-194106/glau-retail-redis
-export cloudrun_east=glau-retail-svc-east1
-export cloudrun_west=glau-retail-svc-west1
+export cloudrun_east=glau-retail-svc-east1-09
+export cloudrun_west=glau-retail-svc-west1-09
 
 # Update env_vars_us_east1.yaml & env_vars_us_westt1.yaml before running the command below
 
@@ -63,3 +63,52 @@ gcloud compute network-endpoint-groups create $neg_west \
         --network-endpoint-type=serverless \
         --cloud-run-service=$cloudrun_west
 
+
+#
+# Create backend service
+#
+
+export lb_ipv4=glau-retail-lb-ip-09
+gcloud compute addresses create $lb_ipv4 \
+	--ip-version=IPV4 \
+	--network-tier=PREMIUM \
+	--global
+
+# gcloud compute addresses describe $lb_ipv4 \
+# --format="get(address)" \
+# --global
+
+
+export backend_svc=glau-retail-backend-svc-09
+
+gcloud compute backend-services create $backend_svc \
+	--global
+#	--protocol=HTTPS \
+#	--port-name=
+
+gcloud compute backend-services add-backend $backend_svc \
+	--network-endpoint-group=$neg_east \
+	--network-endpoint-group-region=us-east1 \
+	--global
+	
+gcloud compute backend-services add-backend $backend_svc \
+        --network-endpoint-group=$neg_west \
+        --network-endpoint-group-region=us-west1 \
+        --global
+
+export url_map=glau-retail-lb-09
+gcloud compute url-maps create $url_map \
+	--default-service=$backend_svc \
+	--global
+
+export http_lb_proxy=glau-retail-http-lb-proxy-09
+gcloud compute target-http-proxies create $http_lb_proxy \
+	--url-map=$url_map
+
+export http_content_rule=glau-retail-http-content-rule-09
+gcloud compute forwarding-rules create http-content-rule \
+	--load-balancing-scheme=EXTERNAL \
+	--address=$lb_ipv4 \
+	--global \
+	--target-http-proxy=$http_lb_proxy \
+	--ports=80
